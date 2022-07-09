d22d862ee0287d4b746201a46284af1ff92fb2d7
lundbmp <m.lundberg008@gmail.com>
2022-07-09 13:02:06 -0500

    added index generating capabilities

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const createFile = require('./dist/fileWriter');
+const indexGenerator = require('./dist/indexGenerator');
+const engineer = require('./lib/Engineer');
+const Engineer = engineer.Engineer;
 
 const htmlPath = "./public/index.html";
-const cssPath = "./public/assets/css/style.css"; 
+const cssPath = "./public/assets/css/style.css";
 
-createFile(htmlPath, "hello world");
+
+// data must be an array of objects
+createFile(htmlPath, indexGenerator(data));
 createFile(cssPath, "hello world");
