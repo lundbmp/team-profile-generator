4cfb0aa4163f4ab3454a7ca59dc96ee408b46680
lundbmp <m.lundberg008@gmail.com>
2022-07-09 14:06:24 -0500

    added css

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const createFile = require('./dist/fileWriter');
 const indexGenerator = require('./dist/indexGenerator');
+const cssGenerator = require('./dist/cssGenerator');
 const engineer = require('./lib/Engineer');
 const Engineer = engineer.Engineer;
 
 const htmlPath = "./public/index.html";
 const cssPath = "./public/assets/css/style.css";
+const data = [new Engineer({name: "mikey", id: "4", email: "mk@mc.com", github: "lunbmp"})];
 
 
 // data must be an array of objects
 createFile(htmlPath, indexGenerator(data));
-createFile(cssPath, "hello world");
+createFile(cssPath, cssGenerator());
