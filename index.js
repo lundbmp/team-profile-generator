eb2aa6aa423868a93bce8d74284b4a15d0f13bcb
lundbmp <m.lundberg008@gmail.com>
2022-07-10 10:29:12 -0500

    started on questions

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const inquirer = require('inquirer');
+
 const createFile = require('./dist/fileWriter');
 const indexGenerator = require('./dist/indexGenerator');
 const cssGenerator = require('./dist/cssGenerator');
+const questions = require('./src/questions');
 const engineer = require('./lib/Engineer');
 const Engineer = engineer.Engineer;
 
 const htmlPath = "./public/index.html";
 const cssPath = "./public/assets/css/style.css";
+const managerQuestions = questions.manager;
+const engineerQuestions = questions.engineer;
+const internQuestions = questions.intern;
+const addMoreQuestion = questions.more;
+const teamArray = [];
 const data = [new Engineer({name: "mikey", id: "4", email: "mk@mc.com", github: "lunbmp"})];
 
 
---
+++ src/questions.js
@@ -0,0 +1,193 @@
+const managerQuestions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'What is your employee ID?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your employee ID!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your email!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is your office number?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your office number!');
+          return false;
+        }
+    }
+}];
+
+const internQuestions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'What is your employee ID?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your employee ID!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your email!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'school',
+    message: 'What is your school name?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your school name!');
+          return false;
+        }
+    }
+}];
+
+const engineerQuestions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'What is your employee ID?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your employee ID!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your email!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is your office number?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your office number!');
+          return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your github name?',
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your github name!');
+          return false;
+        }
+    }
+}];
+
+const addMoreQuestion = [{
+    type: 'list',
+    name: 'addMore',
+    message: 'would you like to add to your team?',
+    choices: ['engineer', 'intern', 'no'],
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('please select an option');
+            return false;
+        }
+    }
+}];
+
+module.exports = {
+    manager: managerQuestions,
+    intern: internQuestions,
+    engineer: engineerQuestions,
+    more: addMoreQuestion,
+};
