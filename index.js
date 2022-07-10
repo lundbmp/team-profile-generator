2ed58ad418a7c8eb10ec9aa41e8cfcf3c4225a16
lundbmp <m.lundberg008@gmail.com>
2022-07-10 11:31:48 -0500

    adding comments, cleaning up redundant code, clearer naming

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// importing inquirer
 const inquirer = require('inquirer');
 
-const createFile = require('./dist/fileWriter');
+// importing required functions, variables, and objects
+const fileWriter = require('./dist/fileWriter');
 const indexGenerator = require('./dist/indexGenerator');
 const cssGenerator = require('./dist/cssGenerator');
 const questions = require('./src/questions');
@@ -11,21 +13,28 @@ const Engineer = engineer.Engineer;
 const intern = require('./lib/Intern');
 const Intern = intern.Intern;
 
+// pathing for fileWriter
 const htmlPath = "./public/index.html";
 const cssPath = "./public/assets/css/style.css";
+// declaring question arrays
 const managerQuestions = questions.manager;
 const engineerQuestions = questions.engineer;
 const internQuestions = questions.intern;
 const addMoreQuestion = questions.more;
-const teamArray = [];
 
+// async function to prompt the user via terminal
 async function inquirerPrompt() {
     let askToAdd;
+    const teamArray = [];
 
+    // prompt initial manager question
     const manager = await inquirer.prompt(managerQuestions);
+    // add manager object to teamArray
     teamArray.push(new Manager(manager));
+    // ask if there is aditional team members
     askToAdd = await askToAddMore();
 
+    // keeps prompting user to add more team members until no is selected
     while(askToAdd.addMore !== 'no') {
         switch(askToAdd.addMore) {
             case 'engineer':
@@ -45,24 +54,23 @@ async function inquirerPrompt() {
 
         askToAdd = await askToAddMore();
     }
-    await asyncCreateFile();
+    // awaiting to create file once prompt is done
+    await createFile(teamArray);
 };
 
+// ask if there is aditional team members
 async function askToAddMore() {
     const result = inquirer.prompt(addMoreQuestion);
 
     return result;
 };
 
-async function asyncCreateFile() {
+// creates html and css file
+async function createFile(teamArray) {
     // data must be an array of objects
-    createFile(htmlPath, indexGenerator(teamArray));
-    createFile(cssPath, cssGenerator());
+    fileWriter(htmlPath, indexGenerator(teamArray));
+    fileWriter(cssPath, cssGenerator());
 }
 
-
+// runs program
 inquirerPrompt();
-
-// data must be an array of objects
-// createFile(htmlPath, indexGenerator(teamArray));
-// createFile(cssPath, cssGenerator());
